b7e072b6356c6daaba3fcb02352b247ab0767aa1
Thomas Iwainski <tiwainski@warburnestate.com.au>
2016-11-21 10:48:47 +1100

    Initial Upload

---
+++ Utilities.Logic/SingleInstance.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
+
+namespace TIUtilities.Logic
+{
+
+    public class SingleInstance : IDisposable
+    {
+        public SingleInstance(TimeSpan timeOut)
+            : this(timeOut,
+                Assembly.GetExecutingAssembly())
+        {
+        }
+
+        public SingleInstance(int timeoutMilliseconds)
+            : this(TimeSpan.FromMilliseconds(timeoutMilliseconds),
+                Assembly.GetExecutingAssembly())
+        {
+        }
+
+        public SingleInstance(int timeoutMilliseconds, Assembly assembly)
+            : this(TimeSpan.FromMilliseconds(timeoutMilliseconds),
+                assembly)
+        {
+        }
+
+        public SingleInstance(TimeSpan timeOut, Assembly executingAssembly)
+        {
+            _executingAssembly = executingAssembly;
+
+            InitMutex();
+            try
+            {
+                HasHandle = _mutex.WaitOne(timeOut < TimeSpan.Zero ? Timeout.InfiniteTimeSpan : timeOut, false);
+
+                if (HasHandle == false)
+                    throw new TimeoutException("Timeout waiting for exclusive access on SingleInstance");
+            }
+            catch (AbandonedMutexException)
+            {
+                HasHandle = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_mutex == null) return;
+            if (HasHandle)
+                _mutex.ReleaseMutex();
+            _mutex.Dispose();
+        }
+
+        private void InitMutex()
+        {
+            var appGuid =
+                ((GuidAttribute) _executingAssembly.GetCustomAttributes(typeof (GuidAttribute), false).GetValue(0))
+                    .Value;
+            var mutexId = $"Global\\{{{appGuid}}}";
+            _mutex = new Mutex(false, mutexId);
+
+            var allowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+                MutexRights.FullControl, AccessControlType.Allow);
+            var securitySettings = new MutexSecurity();
+            securitySettings.AddAccessRule(allowEveryoneRule);
+            _mutex.SetAccessControl(securitySettings);
+        }
+
+        #region  FIELDS
+
+        private readonly Assembly _executingAssembly;
+        private Mutex _mutex;
+        public bool HasHandle;
+
+        #endregion
+    }
+}
---
+++ Utilities.UnitTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TIUtilities.UnitTests
+{
+    [TestFixture]
+    public class SingleInstanceUnitTests
+    {
+
+        [Test]
+        public void UnitAtTest_Conditions_ExpectedBehaviour()
+        {
+            //ARRANGE
+            
+            //ACT
+            //ASSERT
+        }
+    }
+}
